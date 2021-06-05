603dc565ace3288aa88828cced0c8d9193a3a86e
Mike Keogh <mike.keogh@dovetailstudios.com>
2021-06-05 15:14:44 +1200

    higher order structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This will be a react app
+      </header>
+    </div>
+  );
+}
+
+export default App;
